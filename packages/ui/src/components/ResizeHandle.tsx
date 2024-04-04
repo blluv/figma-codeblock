2f9b4bc70bc45c9d022eff84bf4b211894f2718f
blluv <biluv@protonmail.com>
2024-04-04 17:54:01 +0900

    빠르게 리사이징을 할 때 pointerUp을 인식하지 못하는 문제 수정

--- packages/ui/src/components/ResizeHandle.tsx
+++ packages/ui/src/components/ResizeHandle.tsx
@@ -1,28 +1,41 @@
 import styles from "./ResizeHandle.module.css"
 import resizeHandleIcon from "../assets/resize-handle.svg"
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export function ResizeHandle() {
-    const [state, setState] = useState<{ down: boolean }>({ down: false })
+    const [state, setState] = useState<{ down: boolean, pointerId?: number }>({ down: false });
+    const ref = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        const callback = (e: PointerEvent) => {
+            if (!ref.current) return;
+
+            console.log("[ResizeHandle] PointerUp", e.pointerId)
+            ref.current.releasePointerCapture(e.pointerId)
+            setState({ down: false })
+        }
+        window.addEventListener("pointerup", callback);
+        return () => window.removeEventListener("pointerup", callback)
+    }, [state])
 
     const onDown = (e: React.PointerEvent) => {
+        if (!ref.current) return
+
+        console.log("[ResizeHandle] PointerDown", e.pointerId)
         e.currentTarget.setPointerCapture(e.pointerId)
-        setState({ down: true })
+        setState({ down: true, pointerId: e.pointerId })
     }
-    const onUp = (e: React.PointerEvent) => {
-        e.currentTarget.releasePointerCapture(e.pointerId)
-        setState({ down: false })
-    }
+
     const onMove = (e: React.PointerEvent) => {
-        if (state.down) {
-            const width = Math.max(100, Math.floor(e.clientX))
-            const height = Math.max(100, Math.floor(e.clientY))
+        if (state.down && e.pointerId === state.pointerId) {
+            const width = Math.max(100, Math.floor(e.clientX) + 10)
+            const height = Math.max(100, Math.floor(e.clientY) + 10)
 
             parent.postMessage({ pluginMessage: { type: "resize", size: { width, height } } }, "*")
         }
     }
 
-    return <div className={styles.wrap} onPointerDown={onDown} onPointerUp={onUp} onPointerMove={onMove}>
+    return <div ref={ref} className={styles.wrap} onPointerDown={onDown} onPointerMove={onMove}>
         <img className={styles.icon} src={resizeHandleIcon} alt="resize-handle" draggable={false} />
     </div>
 }
